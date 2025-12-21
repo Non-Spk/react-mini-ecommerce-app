f9627846f97e77fbbdc533f03b975bc1b794c5d2
supakpong <149934366+Non-Spk@users.noreply.github.com>
2025-12-21 13:01:34 +0700

    add global theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import AppRouter from "@/router/AppRouter";
 
 export default function App() {
     return (
-        <>
-            <div className="min-h-screen bg-gray-100 text-gray-900 flex flex-col">
-                <AppRouter />
-            </div>
-        </>
+        <div className="min-h-screen bg-background text-text flex flex-col">
+            <AppRouter />
+        </div>
     );
 }
